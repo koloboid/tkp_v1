81946a19e59114cb7d9525548fdfe8fc9cb5ca48
Alexey Hodko <koloboid@gmail.com>
2022-06-04 15:31:33 +0300

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TKP_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(668, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 475);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a = ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numericUpDown1.Location = new System.Drawing.Point(43, 474);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 18);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(389, 466);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(291, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 15;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 503);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.MinimumSize = new System.Drawing.Size(486, 286);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TKP_V1
+{
+    public partial class Form1 : Form
+    {
+        const double b = 1;
+        private Pen axisPen = new Pen(Color.Black);
+        private Pen borderPen = new Pen(Color.Black);
+        private Pen gridPen = new Pen(Color.LightGray);
+        private Pen tickPen = new Pen(Color.Black);
+        private Pen linePen = new Pen(Color.Teal);
+        private Brush textBrush = Brushes.Black;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int axisOffset = 20;
+            int h = pictureBox1.Height - axisOffset;
+            int w = pictureBox1.Width - axisOffset;
+            int tick = 5;
+            Font tickFont = Font;
+            int scale = trackBar1.Value * 5;
+            Graphics g = e.Graphics;
+
+            g.Clear(pictureBox1.BackColor);
+
+            g.TranslateTransform(axisOffset, 0);
+            int xt = 0;
+            for (int x = w / 2; x < w; x += scale) {
+                g.DrawLine(gridPen, x, 0, x, h);
+                g.DrawLine(gridPen, w - x, 0, w - x, h);
+
+                g.DrawLine(tickPen, x, 0, x, tick);
+                g.DrawLine(tickPen, w - x, 0, w - x, tick);
+
+                g.DrawLine(tickPen, x, h, x, h - tick);
+                g.DrawLine(tickPen, w - x, h, w - x, h - tick);
+
+                float tw = g.MeasureString(xt.ToString(), tickFont).Width;
+                g.DrawString(xt.ToString(), tickFont, textBrush, x - tw / 2, h);
+                tw = g.MeasureString((-xt).ToString(), tickFont).Width;
+                g.DrawString((-xt).ToString(), tickFont, textBrush, w - x - tw / 2, h);
+                xt++;
+            }
+            int yt = 0;
+            for (int y = h / 2; y < h; y += scale)
+            {
+                g.DrawLine(gridPen, 0, y, w, y);
+                g.DrawLine(gridPen, 0, h - y, w, h - y);
+
+                g.DrawLine(tickPen, 0, y, tick, y);
+                g.DrawLine(tickPen, 0, h - y, tick, h - y);
+
+                g.DrawLine(tickPen, w, y, w - tick, y);
+                g.DrawLine(tickPen, w, h - y, w - tick, h - y);
+
+                g.DrawString((-yt).ToString(), tickFont, textBrush, -axisOffset, y - tickFont.Height / 2);
+                g.DrawString(yt.ToString(), tickFont, textBrush, -axisOffset, h - y - tickFont.Height / 2);
+                yt++;
+            }
+            g.ResetTransform();
+
+            g.TranslateTransform(axisOffset + w / 2, h / 2);
+            g.SetClip(new Rectangle(-w / 2, -h / 2, w, h));
+            var points = new List<PointF>();
+            for (double t = 0; t < 2 * Math.PI; t += Math.PI / 720)
+            {
+                points.Add(new PointF((float)getX(t) * scale, (float)getY(t) * scale));
+            }
+            g.DrawLines(linePen, points.ToArray());
+            g.ResetTransform();
+            g.ResetClip();
+
+            g.DrawRectangle(borderPen, axisOffset, 0, w - borderPen.Width, h);
+        }
+
+        private double getX(double t)
+        {
+            return 2 * (double)numericUpDown1.Value * Math.Pow(Math.Cos(t), 2) + b * Math.Cos(t);
+        }
+
+        private double getY(double t)
+        {
+            return 2 * (double)numericUpDown1.Value * Math.Sin(t) * Math.Cos(t) + b * Math.Sin(t);
+        }
+    }
+}
